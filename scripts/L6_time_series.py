dfa2ed1feebe8618b8ff4cfa63b25fb17af9d814
danmet <danielmetzen@outlook.com>
2019-04-08 08:29:47 +1000

    housekeeping

--- scripts/L6_time_series.py
+++ scripts/L6_time_series.py
@@ -2,18 +2,7 @@ import xarray as xr
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-def nc_to_df(netcdf_file):
-    """
-    Helper function to load .nc file into pandas dataframe.
-    """
-    ds = xr.open_dataset(netcdf_file)
-    df = ds.to_dataframe()
-    # remove lat, lon from index
-    df = df.reset_index([0, 1])
-    df = df.replace(-9999, np.nan)
-    return df
+from utils import nc_to_df
 
 
 def plot_variable(data, var, ax, window):
