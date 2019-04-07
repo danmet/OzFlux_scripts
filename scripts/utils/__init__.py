1635528e2e626ed335ffbc69db9267bae4bd649a
danmet <danielmetzen@outlook.com>
2019-04-08 08:39:00 +1000

    add concat function

--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import xarray as xr
 import numpy as np
+from pathlib import Path
 
 
 def nc_to_df(netcdf_file):
@@ -13,3 +14,14 @@ def nc_to_df(netcdf_file):
     df = df.reset_index([0, 1])
     df = df.replace(-9999, np.nan)
     return df
+
+
+def concat_data(data_path, pattern):
+    """
+    Helper function to merge .nc file into one pandas dataframe.
+    """
+    data = pd.DataFrame()
+    for f in data_path.glob(pattern):
+        df = nc_to_df(f)
+        data = pd.concat([data, df], sort=False)
+    return data.sort_index()
