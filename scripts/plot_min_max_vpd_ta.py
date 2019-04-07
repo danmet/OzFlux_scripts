2b62e1a85d93ec5748f62676688b9de90f9d92b0
danmet <danielmetzen@outlook.com>
2019-04-08 08:40:35 +1000

    removed util functions

--- scripts/plot_min_max_vpd_ta.py
+++ scripts/plot_min_max_vpd_ta.py
@@ -3,28 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
-
-
-def nc_to_df(netcdf_file):
-    """
-    Helper function to load .nc file into pandas dataframe.
-    """
-    df = xr.open_dataset(netcdf_file)
-    df = df.to_dataframe()
-    df = df.reset_index([0, 1])
-    df = df.replace(-9999, np.nan)
-    return df
-
-
-def concat_data(data_path, pattern):
-    """
-    Helper function to merge .nc file into one pandas dataframe.
-    """
-    data = pd.DataFrame()
-    for f in data_path.glob(pattern):
-        df = nc_to_df(f)
-        data = pd.concat([data, df], sort=False)
-    return data.sort_index()
+from utils import nc_to_df, concat_data
 
 
 def plot_it(data, window, outfile=None):
