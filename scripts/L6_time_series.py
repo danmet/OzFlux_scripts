e73c21d7db2f47722363d9ee37c5a3d893a47ce4
danmet <danielmetzen@outlook.com>
2019-04-08 15:34:29 +1000

    housekeeping

--- scripts/L6_time_series.py
+++ scripts/L6_time_series.py
@@ -53,9 +53,5 @@ def plot_time_series(netcdf_file, window, outfile=None):
         plt.savefig(outfile, dpi=300, bbox_inches='tight')
 
 
-def main():
-    plot_time_series('./data/CumberlandPlain_2018_L6.nc', window=7)
-
-
 if __name__ == '__main__':
-    main()
+    plot_time_series('./data/CumberlandPlain_2018_L6.nc', window=7)
